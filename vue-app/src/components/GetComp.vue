e035e199598a9d9ec55509f67e0fd7c410a247b1
ramo <ramo@ramo.local>
2019-12-17 15:46:23 +0900

    デザインの変更など

--- vue-app/src/components/GetComp.vue
+++ vue-app/src/components/GetComp.vue
@@ -1,26 +1,76 @@
 <template>
     <div class="getcomp">
-        {{ info }}
+        <div class="flex form-box">
+            <p>your name:</p>
+            <input v-model="msg">
+            <button v-on:click="serch(msg)">Greet</button>
+        </div>
+
+        <div class="flex space">
+            <div class="card"  v-for="data in info" :key="data.name">
+                <p>名前:{{data.Name}}</p>
+                <p>ニックネーム:{{data.Nickname}}</p>
+                <p>Mgram:{{data.Mgram}}</p>
+                <p>Twitter:{{data.TwitterId}}</p>
+                <p>所属チーム:{{data.BlongTeam}}</p>
+                <p>住み:{{data.Where}}</p>
+                <p>一言:{{data.Word}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios';
     export default {
+
         name: "GetComp",
         data () {
             return {
-                info: null
+                info: null,
+                msg:"",
             }
         },
-        mounted () {
-            axios
-                .get('http://localhost:80/test')
-                .then(response => (this.info = response))
+        // mounted () {
+        //     axios
+        //         .get('http://localhost:80/test')
+        //         .then(response => (this.info = response.data))
+        // },
+        methods: {
+            greet: function (event) {
+                this.msg = "anker"
+                // `event` は、ネイティブ DOM イベントです
+                if (event) {
+                    alert(event.target.tagName)
+                }
+            },
+            serch: function () {
+                // alert('Hello ' + this.msg + '!')
+                axios.get('http://localhost:80/test')
+                    .then(response => (this.info = response.data))
+                this.msg = ""
+            }
         }
     }
 </script>
 
 <style scoped>
+    .flex{
+        display: flex;
+        /*flex-direction: column;*/
+    }
+    .dlex p{
+        display: flex;
+    }
+    .space{
+        justify-content: space-evenly;
+    }
+    .card{
+        border: 1px solid;
+    }
+    .form-box{
+        padding-bottom: 10%;
+        justify-content: center;
+    }
 
 </style>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="home">
 <!--    <img alt="Vue logo" src="../assets/logo.png">-->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
       <GetComp></GetComp>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import GetComp from "../components/GetComp";
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    // HelloWorld,
       GetComp
   }
 }
